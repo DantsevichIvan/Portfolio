2100f173cefdcfae96d0e53a6df4f0cdd53797a5
DantsevichIvan <iw.dantsevich@gmail.com>
2020-03-02 17:44:09 +0300

    Updates

---
+++ precache-manifest.adc9ef1f40a691eec4f36a1918e0724b.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e7aaaf724ea60df93c28022adb9be2b",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "51892bab2522623afb27",
+    "url": "/Portfolio/static/css/2.c2d4b67f.chunk.css"
+  },
+  {
+    "revision": "aaff2bac6bba00485307",
+    "url": "/Portfolio/static/css/main.2c786856.chunk.css"
+  },
+  {
+    "revision": "51892bab2522623afb27",
+    "url": "/Portfolio/static/js/2.9e8ecb76.chunk.js"
+  },
+  {
+    "revision": "aaff2bac6bba00485307",
+    "url": "/Portfolio/static/js/main.087b9d5d.chunk.js"
+  },
+  {
+    "revision": "02bb7fa0c0195f9aa52b",
+    "url": "/Portfolio/static/js/runtime~main.aeacf694.js"
+  },
+  {
+    "revision": "270f55fffea36219b66a63fbe03ff497",
+    "url": "/Portfolio/static/media/1200px-Node.js_logo.svg.270f55ff.png"
+  },
+  {
+    "revision": "c081f2c69a8ee480a094dc58b99e5f5a",
+    "url": "/Portfolio/static/media/MERN.c081f2c6.jfif"
+  },
+  {
+    "revision": "f0233cc72bb653e7ecf264bf69d57496",
+    "url": "/Portfolio/static/media/What-Is-A-Full-Stack-Developer-and-Everything-You-Need-to-Know-to-Start.f0233cc7.jpg"
+  },
+  {
+    "revision": "1d965015435d8d062b31164b9f654eac",
+    "url": "/Portfolio/static/media/facebook-logo.1d965015.svg"
+  },
+  {
+    "revision": "f67be5aff6fb66e6fc46cbfe5fc769a8",
+    "url": "/Portfolio/static/media/github.f67be5af.svg"
+  },
+  {
+    "revision": "161a3fd125fa75e8dc35757b1ff6e3e3",
+    "url": "/Portfolio/static/media/img_project_1_mono.161a3fd1.png"
+  },
+  {
+    "revision": "4e04ea3fc888e1ef1d3038491a5b663d",
+    "url": "/Portfolio/static/media/linkedin-letters.4e04ea3f.svg"
+  },
+  {
+    "revision": "387eb7e06855799f1db9cd3f0fc9c6eb",
+    "url": "/Portfolio/static/media/message.387eb7e0.svg"
+  },
+  {
+    "revision": "c2c66675d459d153dd11254ebff55966",
+    "url": "/Portfolio/static/media/phone-call.c2c66675.svg"
+  },
+  {
+    "revision": "415f32d0c751a2c1a2f3469d6ce71d3c",
+    "url": "/Portfolio/static/media/placeholder.415f32d0.svg"
+  },
+  {
+    "revision": "a7000e40811d2e8b290d2cc167bab0e8",
+    "url": "/Portfolio/static/media/programmer_1505290086-1140x570.a7000e40.jpg"
+  },
+  {
+    "revision": "e678dfc4ad25acf543a40efd0afe158b",
+    "url": "/Portfolio/static/media/telegram.e678dfc4.svg"
+  },
+  {
+    "revision": "b3912abe9411dcbcc7fa05f993f44d5f",
+    "url": "/Portfolio/static/media/vk.b3912abe.svg"
+  }
+]);
